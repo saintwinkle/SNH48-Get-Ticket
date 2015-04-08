4ada8f82e10dffde16d5473f5c4d0db0eccd4f83
saintwinkle <saintwinkle@gmail.com>
2015-04-08 16:11:45 +0800

    v0.2.1(always forget to change)

--- SNH48-Get-Ticket.user.js
+++ SNH48-Get-Ticket.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          SNH48 Get Ticket
 // @namespace     http://weibo.com/1997375853
-// @version       0.2.0
+// @version       0.2.1
 // @author        Twinkle
 // @description   SNH48官方商城抢票脚本
 // @include       http://shop.snh48.com/goods-*
